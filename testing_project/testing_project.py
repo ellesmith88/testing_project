8d9e8277db8f208ce79494d30fb728266f78ebcb
ellesmith88 <e.s.smith@hotmail.co.uk>
2020-12-01 10:47:17 +0000

    Added function

--- testing_project/testing_project.py
+++ testing_project/testing_project.py
@@ -4,3 +4,8 @@ __author__ = """Elle Smith"""
 __contact__ = "eleanor.smith@stfc.ac.uk"
 __copyright__ = "Copyright 2020 United Kingdom Research and Innovation"
 __license__ = "BSD - see LICENSE file in top-level package directory"
+
+
+def my_function():
+    """ Function to return Hello World"""
+    return "Hi World"
--- tests/test_testing_project.py
+++ tests/test_testing_project.py
@@ -7,20 +7,9 @@ __contact__ = "eleanor.smith@stfc.ac.uk"
 __copyright__ = "Copyright 2020 United Kingdom Research and Innovation"
 __license__ = "BSD - see LICENSE file in top-level package directory"
 
-import pytest
+from testing_project.testing_project import my_function
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_my_function():
+    resp = my_function()
+    assert resp
